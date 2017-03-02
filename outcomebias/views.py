1ed718ae8bea56e0002b7f516acaf9a096f23e5d
chkgk <koenig.kersting@gmail.com>
2017-03-02 09:47:40 +0100

    local version

--- outcomebias/views.py
+++ outcomebias/views.py
@@ -17,27 +17,42 @@ class Arrival(WaitPage):
 		self.group.set_treatment()
 
 class Welcome(Page):
-	pass
+	timeout_seconds = Constants.long_timeout
 
-class Instructions(Page):
-	pass
-
-class RoleAssignment(Page):
-	pass
+	def before_next_page(self):
+		if self.timeout_happened:
+			self.player.timed_out = True
+			self.group.intact = False
 
 class WinningColorChoice(Page):
 	form_model = models.Group
 	form_fields = ['winning_color']
 
+	timeout_seconds = Constants.decision_timeout
+	timeout_submission = { 'winning_color': 'none' }
+
+	def before_next_page(self):
+		if self.timeout_happened:
+			self.player.timed_out = True
+			self.group.intact = False
+
 	def is_displayed(self):
-		return self.group.treatment == 'agent' and self.player.role() == 'agent'
+		return self.group.intact and self.group.treatment == 'agent' and self.player.role() == 'agent'
 
 class RewardDecision(Page):
 	form_model = models.Group
 	form_fields = ['reward_good', 'reward_bad']
 
+	timeout_seconds = Constants.decision_timeout
+	timeout_submission = {'reward_good': 0, 'reward_bad': 0}
+
+	def before_next_page(self):
+		if self.timeout_happened:
+			self.player.timed_out = True
+			self.group.intact = False
+
 	def is_displayed(self):
-		return self.player.role() == 'principal'
+		return self.group.intact and self.player.role() == 'principal'
 
 class ResultsWaitPage(WaitPage):
 	template_name = 'outcomebias/MyWaitPage.html'
@@ -47,16 +62,19 @@ class ResultsWaitPage(WaitPage):
 		self.group.set_payoffs()
 
 class Results(Page):
-	pass
+	def is_displayed(self):
+		return self.group.intact
 
+class Failure(Page):
+	def is_displayed(self):
+		return not self.group.intact 
 
 page_sequence = [
 	Arrival,
 	Welcome,
-	#Instructions,
-	#RoleAssignment,
 	WinningColorChoice,
 	RewardDecision,
 	ResultsWaitPage,
-	Results
+	Results,
+	Failure
 ]
